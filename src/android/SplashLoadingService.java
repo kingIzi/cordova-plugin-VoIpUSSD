8fc7145f5222e39eba648dea720e2e109cec0bbe
Izi Kapampa <56266013+kingIzi@users.noreply.github.com>
2025-04-01 11:42:36 +0300

    Update SplashLoadingService.java

--- src/android/SplashLoadingService.java
+++ src/android/SplashLoadingService.java
@@ -18,6 +18,9 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
+import android.app.Application;
+import android.content.res.Resources;
+
 /**
  * SplashLoadingService for Android splashing dialog
  *
@@ -52,7 +55,7 @@ public class SplashLoadingService extends Service {
         int padding_in_px = (int) (padding_in_dp * scale + 0.5f);
 
         layout = new LinearLayout(this);
-        layout.setBackgroundColor(R.color.blue_background);
+        //layout.setBackgroundColor(R.color.blue_background);
         layout.setOrientation(LinearLayout.VERTICAL);
 
         WindowManager.LayoutParams params =
@@ -65,7 +68,7 @@ public class SplashLoadingService extends Service {
                                 PixelFormat.RGB_565);
 
         ImageView imageView = new ImageView(this);
-        imageView.setImageResource(R.drawable.favico);
+        //imageView.setImageResource(R.drawable.favico);
         imageView.setPaddingRelative(0,padding_in_px,0,padding_in_px);
         LinearLayout.LayoutParams params_ll = new LinearLayout
                 .LayoutParams(LinearLayout.MarginLayoutParams.MATCH_PARENT, 0);
@@ -81,7 +84,7 @@ public class SplashLoadingService extends Service {
         layout.addView(relativeLayout, params_ll);
 
         GifImageView gifImageView = new GifImageView(this);
-        gifImageView.setGifImageResource(R.drawable.loading);
+        //gifImageView.setGifImageResource(R.drawable.loading);
         gifImageView.setPaddingRelative(0,padding_in_px,0,padding_in_px);
 
         relativeLayout = new RelativeLayout(this);
